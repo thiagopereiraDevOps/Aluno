d5594720014b1c883a69e968c0794c23f1450e46
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-03-27 16:20:37 -0300

    Meus arquivos de Aluno

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setNome("Farmácia");
+        curso.setHoras(3600);
+        curso.setTurno(Turno.MANHA);
+
+        System.out.println("Curso " + curso.getNome() + " disponível no turno da " + curso.getTurno().getDescricao());
+    }
+}
---
+++ src/Turno.java
@@ -0,0 +1,14 @@
+public enum Turno {
+    MANHA("manhã"),
+    TARDE("tarde"),
+    NOITE("noite");
+
+    private String descricao;
+    Turno(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
